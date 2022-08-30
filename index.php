88a083e25390c4e270cdd7761d24078a1c78ad56
marcocalligaris <marcocally@gmail.com>
2022-08-30 08:58:00 +0200

    Exercise done

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php 
+$text = 'How much wood would a woodchuck chuck if a woodchuck could chuck wood?';
+$text_array = explode(' ', $text);
+var_dump($text_array);
+$badword = $_GET['name'];
+$new_text = str_replace($badword, '***', $text);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Badwords</title>
+</head>
+<body>
+    <h3><?php echo $text ?></h3>
+    <h2>Length: <?php echo strlen($text) ?> characters</h2>
+    <h1>New text: <?php echo $new_text ?></h1>
+    <h2>New length: <?php echo strlen($new_text) ?> characters</h2>
+</body>
+</html>
